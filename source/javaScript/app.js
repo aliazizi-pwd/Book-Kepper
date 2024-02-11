16926a0f16c599b1a1d793dbba70083f928b881e
ali azizi <aliazizi.4973@gmail.com>
2024-02-11 12:38:31 +0330

    select element's html

---
+++ source/javaScript/app.js
@@ -0,0 +1,20 @@
+// *Get start code javaScript project* //
+// --> variables of let 
+let $ = document;
+// *variables of const*
+// --> input elements selected :)
+const innerID = $.querySelector(".input-id");
+const innerTitle = $.querySelector(".input-title");
+const innerAuthor = $.querySelector(".input-author");
+const innerYear = $.querySelector(".input-year");
+const innerGenre = $.querySelector(".input-genre");
+// --> buttons elements clicked selected :)
+const btnAdd = $.querySelector(".btnAdd");
+const btnClear = $.querySelector(".btn-clear");
+const btnChangeTheme = $.querySelector(".btn-changeTheme");
+// --> select elements change selected :)
+const filterElm = $.querySelector(".filter");
+// --> other elements selected :)
+const countBook = $.querySelector(".count-book");
+const messageDataBase = $.querySelector(".message-dataBase");
+const dataBaseView = $.querySelector(".dataBase-Book");
