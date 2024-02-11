9647344bee232eebb68935cee505d970a1e86a6b
ali azizi <aliazizi.4973@gmail.com>
2024-02-11 14:22:08 +0330

    Developing and writing feature code for website theme changes

--- source/javaScript/app.js
+++ source/javaScript/app.js
@@ -1,6 +1,7 @@
 // *Get start code javaScript project* //
 // --> variables of let 
 let $ = document;
+let themeApp = "light";
 // *variables of const*
 // --> input elements selected :)
 const innerID = $.querySelector(".input-id");
@@ -18,3 +19,47 @@ const filterElm = $.querySelector(".filter");
 const countBook = $.querySelector(".count-book");
 const messageDataBase = $.querySelector(".message-dataBase");
 const dataBaseView = $.querySelector(".dataBase-Book");
+
+
+// function change theme app
+function changeThemeHandler (e) {
+    if (themeApp === "light") {
+        darkModeThemeHandler();
+        themeApp = "dark";
+        localStorage.setItem("theme",themeApp);
+        e.target.innerHTML = `<i class="fa fa-sun"></i>`;
+    } else {
+        lightModeThemeHandler();
+        themeApp = "light";
+        localStorage.setItem("theme",themeApp);
+        e.target.innerHTML = `<i class="fa fa-moon"></i>`;
+    }
+}   
+
+// function dark mode theme handler
+function darkModeThemeHandler () {
+    document.documentElement.style.setProperty("--theme-Background","#000");
+    document.documentElement.style.setProperty("--theme-Color","#fff");
+}
+
+// function dark mode theme handler
+function lightModeThemeHandler () {
+    document.documentElement.style.setProperty("--theme-Background","#fff");
+    document.documentElement.style.setProperty("--theme-Color","#000");
+}
+
+// function load application handler
+function loadAppHandler () {
+    // --> load app for part change theme application 
+    // --> The theme will be dark when the page is loaded
+    let getTheme = localStorage.getItem("theme");
+    if (getTheme === "dark") {
+        darkModeThemeHandler();
+    }
+}
+
+
+// add event listener for element
+btnChangeTheme.addEventListener("click",changeThemeHandler);
+// document add event listener for element body or document
+window.addEventListener("load",loadAppHandler);
